d0b2d37270ef6e9bd88b9d57952195178a66f097
Bendu-Olo <spartan_117@juno.com>
2019-03-23 16:57:42 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -51,8 +51,8 @@ except IOError:
  die(file_message%sys.argv[2])
 
 import socket
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 try:
  sock.bind(("",port))
 except OSError:
